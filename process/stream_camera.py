ffdd3300c25f87086275abe8ec5279c31801921c
Nanthida-Da <dadada.23lovely@gmail.com>
2023-08-04 10:53:42 +0700

    Taa

--- process/stream_camera.py
+++ process/stream_camera.py
@@ -5,37 +5,7 @@ import uvicorn
 
 app = FastAPI()
 
-def gstreamer_pipeline(
-    capture_width=3280,
-    capture_height=2464,
-    display_width=820,
-    display_height=616,
-    framerate=21,
-    flip_method=0,
-):
-    return (
-        "nvarguscamerasrc ! "
-        "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
-
-# camera = cv2.VideoCapture(0)
-camera = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
-
-
+camera = cv2.VideoCapture(0)
 def gen_frames():  
     while True:
         success, frame = camera.read()
